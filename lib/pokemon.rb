c0aaf24a532447b9b445fc0d37627f939ec0c188
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-11-12 19:35:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,6 +18,6 @@ end
 
 def self.find(id, db) 
   new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;" id)
-  Pokemon.new(id: new_pokemon[0])
+  Pokemon.new(id: new_pokemon[0], name: new_pokemon [1], type)
 end
 end
