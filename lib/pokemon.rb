9e4c8a92199984f31a2482ee42d35e1a7e487ce8
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-11-12 15:39:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
+  
+  
+  def initialize (id:, name:, type:, db:)
+    self.id = id
+    self.name = name
+    self.type = type
+    self.db = db
+end
+
+def self.save(name, type, db)
+  
+  
+  
+end
 end
